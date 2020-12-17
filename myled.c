5d21a969c42949ffb28adcbc64c2396782e873b0
takayasakamoto <72371520+sakamoto-takaya@users.noreply.github.com>
2020-12-17 16:14:54 +0900

    Update myled.c

--- myled.c
+++ myled.c
@@ -6,7 +6,7 @@
 #include <linux/io.h>　
 #include <linux/delay.h>
 
-MODULE_AUTHOR("Takaya Sakamoto and Ryuichi Ueda and Yuya hasegawa");
+MODULE_AUTHOR("Takaya Sakamoto and Ryuichi Ueda");
 MODULE_DESCRIPTION("driver for LED control");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.0.1");
